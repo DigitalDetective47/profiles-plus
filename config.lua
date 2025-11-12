65877966f0e5a4e65c96180d632681fa52792f2f
DigitalDetective47 <ninji2701@gmail.com>
2025-11-11 22:27:44 -0500

    made save_order reverse lookup a function

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 return {
     vanilla_compat = true,
-    save_order = { 1, 2, 3, reverse = { ["1"] = 1, ["2"] = 2, ["3"] = 3, New = 4 } },
+    save_order = { "1", "2", "3" },
     modpacks = false,
 }
--- main.lua
+++ main.lua
@@ -1,7 +1,26 @@
-if not SMODS.current_mod.config.save_order.reverse[tostring(G.SETTINGS.profile)] then
-    table.insert(SMODS.current_mod.config.save_order, G.SETTINGS.profile)
-    SMODS.current_mod.config.save_order.reverse[tostring(G.SETTINGS.profile)] = #SMODS.current_mod.config.save_order
-    SMODS.save_mod_config(SMODS.current_mod)
+---@type table<string, integer>
+local save_order_reverse_cache = {}
+---perform a cached reverse lookup on `SMODS.Mods.ProfilesPlus.config.save_order`
+---@param value string
+---@return integer? key returns `nil` if the requested value could not be found
+function SMODS.Mods.ProfilesPlus.save_order_reverse(value)
+    ---@type integer
+    local cached_key = save_order_reverse_cache[value]
+    if SMODS.Mods.ProfilesPlus.config.save_order[cached_key] == value then
+        return cached_key
+    end
+    for k, v in ipairs(SMODS.Mods.ProfilesPlus.config.save_order) do
+        if v == value then
+            save_order_reverse_cache[value] = k
+            return k
+        end
+    end
+    sendErrorMessage(value .. " could not be found in save order.", "ProfilesPlus.save_order_reverse")
+end
+
+if not SMODS.Mods.ProfilesPlus.save_order_reverse(G.SETTINGS.profile) then
+    table.insert(SMODS.Mods.ProfilesPlus.config.save_order, G.SETTINGS.profile)
+    SMODS.save_mod_config(SMODS.Mods.ProfilesPlus)
 end
 
 local profile_option_hook = G.UIDEF.profile_option
@@ -12,12 +31,12 @@ end
 
 ---@param args { from_key: integer, from_val: string, to_key: integer, to_val: string, cycle_config: { [string]: any } }
 function G.FUNCS.scroll_profiles(args)
-    G.focused_profile = tonumber(args.to_val) or "New"
+    G.focused_profile = args.to_val or "New"
     ---@type UIElement
     local profile_swapper = G.OVERLAY_MENU:get_UIE_by_ID("profile_swapper")
     profile_swapper.config.object:remove()
     profile_swapper.config.object = UIBox {
-        definition = G.UIDEF.profile_option(args.to_val == "New" and math.random(128, 2 ^ 32 - 1) or G.focused_profile),
+        definition = G.UIDEF.profile_option(args.to_val == "New" and "pp_" .. math.random(0, 2 ^ 32 - 1) or G.focused_profile),
         config = { parent = profile_swapper } }
     G.OVERLAY_MENU:recalculate()
 end
@@ -26,10 +45,7 @@ end
 function G.UIDEF.profile_select()
     G.focused_profile = G.focused_profile or G.SETTINGS.profile or 1
     ---@type string[]
-    local options = {}
-    for index, id in ipairs(SMODS.Mods.ProfilesPlus.config.save_order) do
-        options[index] = tostring(id)
-    end
+    local options = SMODS.shallow_copy(SMODS.Mods.ProfilesPlus.config.save_order)
     table.insert(options, "New")
     ---@type UIElement
     local profile_swapper = { n = G.UIT.O, config = { id = "profile_swapper" } }
@@ -40,7 +56,7 @@ function G.UIDEF.profile_select()
             config = { align = "cm", padding = 0, draw_layer = 1, minw = 4 },
             nodes = {
                 { n = G.UIT.R, config = { align = "cm", id = "profile_option_cycle" }, nodes = {
-                    create_option_cycle { options = options, current_option = SMODS.Mods.ProfilesPlus.config.save_order.reverse[tostring(G.focused_profile)], opt_callback = "scroll_profiles" } } },
+                    create_option_cycle { options = options, current_option = SMODS.Mods.ProfilesPlus.save_order_reverse(G.focused_profile), opt_callback = "scroll_profiles", w = 3 } } },
                 { n = G.UIT.R, config = { align = "cm" }, nodes = { profile_swapper } },
             }
         } }
