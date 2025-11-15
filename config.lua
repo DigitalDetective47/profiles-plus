c0085ccff4412730783a5d073767db79c34d4686
DigitalDetective47 <ninji2701@gmail.com>
2025-11-14 20:19:03 -0500

    add options for forced visible slots

--- config.lua
+++ config.lua
@@ -1,5 +1,24 @@
+function SMODS.current_mod.config_tab()
+    return {
+        n = G.UIT.ROOT,
+        config = { r = 0.1, colour = G.C.BLACK, emboss = 0.05 },
+        nodes = { {
+            n = G.UIT.C,
+            config = { minw = 7 },
+            nodes = {
+                -- { n = G.UIT.R, nodes = { create_toggle { label = localize("modpacks", "profile_UI"), ref_table = SMODS.Mods.ProfilesPlus.config, ref_value = "modpacks" } } },
+                -- { n = G.UIT.R, config = { minh = 0.4 } },
+                { n = G.UIT.R, nodes = { { n = G.UIT.T, config = { text = localize("permanent_options", "profile_UI"), scale = 0.6, colour = G.C.UI.TEXT_LIGHT } } } },
+                { n = G.UIT.R, nodes = { create_toggle { label = localize("Balatro", "profile_UI"), ref_table = SMODS.Mods.ProfilesPlus.config.permanent_options, ref_value = "Balatro" } } },
+                { n = G.UIT.R, nodes = { create_toggle { label = localize("more_profiles", "profile_UI"), ref_table = SMODS.Mods.ProfilesPlus.config.permanent_options, ref_value = "more_profiles" } } },
+                { n = G.UIT.R, nodes = { create_toggle { label = localize("Cryptid", "profile_UI"), ref_table = SMODS.Mods.ProfilesPlus.config.permanent_options, ref_value = "Cryptid" } } },
+            }
+        } }
+    }
+end
+
 return {
-    vanilla_compat = true,
-    save_order = { "1", "2", "3" },
+    permanent_options = { Balatro = true, more_profiles = false, Cryptid = false },
+    save_order = {},
     modpacks = false,
 }
--- main.lua
+++ main.lua
@@ -18,9 +18,14 @@ function SMODS.Mods.ProfilesPlus.save_order_reverse(value)
     sendErrorMessage(value .. " could not be found in save order.", "ProfilesPlus.save_order_reverse")
 end
 
-if not SMODS.Mods.ProfilesPlus.save_order_reverse(G.SETTINGS.profile) then
+if G.SETTINGS.profile:sub(1, 3) == "pp_" and not SMODS.Mods.ProfilesPlus.save_order_reverse(G.SETTINGS.profile) then
     table.insert(SMODS.Mods.ProfilesPlus.config.save_order, G.SETTINGS.profile)
     SMODS.save_mod_config(SMODS.Mods.ProfilesPlus)
+end
+for index = #SMODS.Mods.ProfilesPlus.config.save_order, 1, -1 do
+    if not love.filesystem.getInfo(SMODS.Mods.ProfilesPlus.config.save_order[index] .. '/profile.jkr') then
+        table.remove(SMODS.Mods.ProfilesPlus.config.save_order, index)
+    end
 end
 
 local profile_option_hook = G.UIDEF.profile_option
@@ -41,12 +46,44 @@ function G.FUNCS.scroll_profiles(args)
     G.OVERLAY_MENU:recalculate()
 end
 
+---@return string[]
+function SMODS.Mods.ProfilesPlus.get_options()
+    ---@type string[]
+    local ret = SMODS.shallow_copy(SMODS.Mods.ProfilesPlus.config.save_order)
+    table.insert(ret, "New")
+    ---@type { id: string, setting: string}[]
+    local permanent_options = { { id = "1", setting = "Balatro" }, { id = "2", setting = "Balatro" }, { id = "3", setting = "Balatro" },
+        { id = "4", setting = "more_profiles" }, { id = "5", setting = "more_profiles" }, { id = "6", setting = "more_profiles" }, { id = "7", setting = "more_profiles" }, { id = "8", setting = "more_profiles" }, { id = "9", setting = "more_profiles" }, { id = "10", setting = "more_profiles" },
+        { id = "M1", setting = "Cryptid" }, { id = "M2", setting = "Cryptid" }, { id = "M3", setting = "Cryptid" } }
+    ---@type integer
+    local ret_index = 1
+    for _, entry in ipairs(permanent_options) do
+        if ret[ret_index] == entry.id then
+            ret_index = ret_index + 1
+        elseif love.filesystem.getInfo(entry.id .. '/profile.jkr') or SMODS.Mods.ProfilesPlus.config.permanent_options[entry.setting] then
+            table.insert(ret, ret_index, entry.id)
+            ret_index = ret_index + 1
+        end
+    end
+    return ret
+end
+
 ---@return UIBox
 function G.UIDEF.profile_select()
-    G.focused_profile = G.focused_profile or G.SETTINGS.profile or 1
+    G.focused_profile = G.focused_profile or G.SETTINGS.profile or "1"
     ---@type string[]
-    local options = SMODS.shallow_copy(SMODS.Mods.ProfilesPlus.config.save_order)
-    table.insert(options, "New")
+    local options = SMODS.Mods.ProfilesPlus.get_options()
+    ---@type integer?
+    local current_option = nil
+    for index, id in ipairs(options) do
+        if id == G.focused_profile then
+            current_option = index
+            break
+        end
+    end
+    if not current_option then
+        sendErrorMessage("Current profile is not in the options switcher!", "ProfilesPlus profile selector")
+    end
     ---@type UIElement
     local profile_swapper = { n = G.UIT.O, config = { id = "profile_swapper" } }
     profile_swapper.config.object = UIBox { definition = G.UIDEF.profile_option(G.focused_profile), config = { parent = profile_swapper } }
@@ -56,7 +93,7 @@ function G.UIDEF.profile_select()
             config = { align = "cm", padding = 0, draw_layer = 1, minw = 4 },
             nodes = {
                 { n = G.UIT.R, config = { align = "cm", id = "profile_option_cycle" }, nodes = {
-                    create_option_cycle { options = options, current_option = SMODS.Mods.ProfilesPlus.save_order_reverse(G.focused_profile), opt_callback = "scroll_profiles", w = 3 } } },
+                    create_option_cycle { options = options, current_option = current_option, opt_callback = "scroll_profiles", w = 3 } } },
                 { n = G.UIT.R, config = { align = "cm" }, nodes = { profile_swapper } },
             }
         } }
